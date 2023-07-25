2c717fd2c8fc3e0d84ac1175a8b9dcfecde13837
Vivek2425 <viveklakhlani.mscit20@vnsgu.ac.in>
2023-07-25 12:09:17 +0530

    ques 2 changes

--- Ques2/app.js
+++ Ques2/app.js
@@ -16,16 +16,17 @@ const static = require('node-static')
 //     console.log("http://localhost:8080");
 // })
 
-var fileserver = static.Server('static');
+var fileserver =new static.Server('static');
 
 const server = http.createServer((req, res) => {
    var url2 = url.parse(req.url, true);
-   if (url2.pathname == "/test.html") {
+   if (url2.pathname == "/") {
       fileserver.serve(req, res);
-   } else if (url2.pathname == "/getHello") {
+   } else if (url2.pathname == "/getHello" && req.method=="GET") {
       res.end("Hello Node js..!!");
+      // res.end();
    } else {
-      res.end("Do nothing")
+      res.end("Do nothing");
    }
 });
 
