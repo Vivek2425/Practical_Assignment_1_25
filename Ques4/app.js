de11f6ff020edda0c945f5f773ff42280a713087
Vivek2425 <viveklakhlani.mscit20@vnsgu.ac.in>
2023-07-25 11:41:31 +0530

    Ques4

--- Ques4/app.js
+++ Ques4/app.js
@@ -5,3 +5,22 @@ const url = require('url');
 const st = require('node-static');
 
 const fileserver = new st.Server('./public');
+
+const httpserver = http.createServer((req,res)=>{
+    req.on('end',()=>{
+        var get  = url.parse(req.url,true).query;
+        fileserver.serve(req,res);
+    }).resume();
+}).listen(8080,()=>{
+    console.log("http://localhost:8080");
+})
+
+const wss = new websocket.Server({server:httpserver});
+
+wss.on('connection',(ws)=>{
+    ws.send("hello client..!!");
+
+    ws.on('message',messgae=>{
+        ws.send('I recieved : ' + messgae)
+    })
+})
